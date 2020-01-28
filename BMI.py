a84e14c0898982ee5a1022c44d8252d885e2b5e7
Richard Deegan <G00387896@gmit.ie>
2020-01-28 14:38:29 +0000

    new BMI file

---
+++ BMI.py
@@ -0,0 +1,11 @@
+# this is a program to calculate a persons BMI 
+
+weight = float (input("enter weight"))
+height = float (input("enter height"))
+
+#height in cm
+#weight in kg
+
+BMI = weight / height
+
+print("your BMI is",BMI)
