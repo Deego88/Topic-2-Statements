0dd2b0e3e135bdcb7aaa8368e67eda001ba73c2a
Deego88 <60344243+Deego88@users.noreply.github.com>
2020-02-28 17:30:01 +0000

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,12 +1,11 @@
-# this is a program to calculate a persons BMI 
+#Richard Deegan
+#This is a program to calculate a persons BMI 
 
-weight = float (input("enter weight kg:")) # asks user to input weight as a float number
-height = float (input("enter height cm:")) # asks user to input height as a float number 
+weight = float (input("enter weight kg:")) # asks user to input weight as a float number, weight in kg
+height = float (input("enter height cm:")) # asks user to input height as a float number, height in cm 
 
-
-#weight in kg
-#height in cm
 heightcmtom = (height/100)
+
 #BMI = weight / height in m2
-BMI= weight / (heightcmtom **2) # raises to the power of 2 for m2
+BMI= weight / (heightcmtom **2) # raises heightcmtom to the power of 2 for m2
 print("your BMI is", BMI) # output string text and BMI result
